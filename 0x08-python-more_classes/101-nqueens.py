e4388624a440021e519115a5458e752d53ec47fc
Nonnyyy <migwilg025@gmail.com>
2023-01-03 22:16:09 +0000

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,7 +4,6 @@
 
 import sys
 
-
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
@@ -12,13 +11,11 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
-
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
-
 
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
@@ -29,7 +26,6 @@ def get_solution(board):
                 solution.append([r, c])
                 break
     return (solution)
-
 
 def xout(board, row, col):
     """X out spots on a chessboard.
@@ -81,7 +77,6 @@ def xout(board, row, col):
         board[r][c] = "x"
         c -= 1
 
-
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
     Args:
@@ -96,7 +91,6 @@ def recursive_solve(board, row, queens, solutions):
         solutions.append(get_solution(board))
         return (solutions)
 
-
     for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
@@ -106,7 +100,6 @@ def recursive_solve(board, row, queens, solutions):
                                         queens + 1, solutions)
 
     return (solutions)
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
