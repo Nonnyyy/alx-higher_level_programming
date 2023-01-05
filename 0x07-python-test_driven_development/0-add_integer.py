1014284aa730f10a28778d7ca9114ff711a4e21e
Nonnyyy <migwilg025@gmail.com>
2023-01-05 18:58:53 +0000

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ This module is composed by a function that adds two numbers
 """
 
 
-def add_integer(a, b=98) :
+def add_integer(a, b=98):
     """ Function that adds two integer and/or float numbers
     Args:
         a: first number
